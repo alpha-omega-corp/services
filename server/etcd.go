149fa882f5466825f2df0ca28a62cd4737556de7
nanstis <bleyo@alphomega.org>
2023-12-27 20:17:51 +0100

    fix: config struct pointer

--- server/etcd.go
+++ server/etcd.go
@@ -2,7 +2,7 @@ package server
 
 import "github.com/spf13/viper"
 
-func NewConfigurationManager(handler *viper.Viper, config string, s struct{}) (err error) {
+func NewConfigurationManager(handler *viper.Viper, config string, s *struct{}) (err error) {
 	err = handler.AddRemoteProvider("etcd3", "http://127.0.0.1:2379", config)
 	if err != nil {
 		return
@@ -11,7 +11,7 @@ func NewConfigurationManager(handler *viper.Viper, config string, s struct{}) (e
 	viper.SetConfigType("yaml")
 
 	err = viper.ReadRemoteConfig()
-	err = viper.Unmarshal(&s)
+	err = viper.Unmarshal(s)
 
 	return
 }
