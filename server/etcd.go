8d82fded7cd1d90d621c6ef6b19ded9bdf92fd56
nanstis <bleyo@alphomega.org>
2023-12-27 20:03:12 +0100

    feat: configuration manager with etcd

---
+++ server/etcd.go
@@ -0,0 +1,17 @@
+package server
+
+import "github.com/spf13/viper"
+
+func NewConfigurationManager(handler *viper.Viper, config string) (err error) {
+	err = handler.AddRemoteProvider("etcd3", "http://127.0.0.1:2379", config)
+	if err != nil {
+		return
+	}
+
+	viper.SetConfigType("yaml")
+
+	err = viper.ReadRemoteConfig()
+	err = viper.Unmarshal(&config)
+
+	return
+}
