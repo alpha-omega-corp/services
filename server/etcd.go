ff57165909a5e4b7f49ab8635b6bf5b1d719ed79
nanstis <bleyo@alphomega.org>
2023-12-27 20:29:16 +0100

    fix: config manager

--- server/etcd.go
+++ server/etcd.go
@@ -2,16 +2,14 @@ package server
 
 import "github.com/spf13/viper"
 
-func NewConfigurationManager(handler *viper.Viper, config string, s *struct{}) (err error) {
+func NewConfigurationManager(handler *viper.Viper, config string) (err error) {
 	err = handler.AddRemoteProvider("etcd3", "http://127.0.0.1:2379", config)
 	if err != nil {
 		return
 	}
 
 	viper.SetConfigType("yaml")
-
-	err = viper.ReadRemoteConfig()
-	err = viper.Unmarshal(s)
+	err = handler.ReadRemoteConfig()
 
 	return
 }
